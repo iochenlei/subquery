79caee3ee03357a955014237f115793af41815b0
chenlei <lei.chen1@thoughtworks.com>
2020-10-21 10:57:24 +0800

    fix lint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 
+
 def define_env(env):
     @env.macro
     def newest_posts():
@@ -9,10 +10,10 @@ def define_env(env):
             if not page.url:
                 continue
             filename = os.path.join(env.variables.config['docs_dir'],
-                page.url[:len(page.url) - 1] + '.md')
+                                    page.url[:len(page.url) - 1] + '.md')
             results.append((os.stat(filename).st_mtime, page))
         results = sorted(results, key=lambda x: x[0], reverse=True)
         output = []
         for modify_time, page in results[:50]:
-            output.append("+ " + datetime.fromtimestamp(modify_time).strftime("%Y-%m-%d %H:%M:%S") + " - <a href=\"" + page.abs_url + "\">" + page.title + "</a>")
+            output.append('+ ' + datetime.fromtimestamp(modify_time).strftime('%Y-%m-%d %H:%M:%S') + ' - <a href=" + page.abs_url + ">' + page.title + '</a>')
         return "\n".join(output)
